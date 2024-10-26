ffd8ff1c9246a29abde7e30e315366ec752e07b7
Ethan <hall.ethan.au@gmail.com>
2024-10-26 18:22:55 +1100

    Imported framework

---
+++ Menus/home.gd
@@ -0,0 +1,6 @@
+class_name Home_Screen
+extends Screen
+
+
+func _on_button_pressed():
+	change_scene("res://Menus/home.tscn")
---
+++ System/system.gd
@@ -0,0 +1,41 @@
+class_name System
+extends Node2D
+
+var current_scene : Screen = null
+@export var starting_address : String
+@onready var cover : ColorRect = $Canvas/Cover
+
+
+func _ready() -> void:
+	cover.modulate.a = 1
+	cover.visible = true
+	change_scene(starting_address)
+
+
+func change_scene(new_add: String, fade_color: Color = Color.BLACK) -> void:
+	var scene_check : Node = load(new_add).instantiate()
+	if scene_check is Screen:
+		var new_scene : Screen = scene_check
+		var fader = get_tree().create_tween()
+		cover.color = fade_color
+		
+		# Fade in the cover
+		cover.visible = true
+		fader.tween_property(cover,"modulate:a",1,0.5)
+		fader.tween_property(cover,"modulate:a",0,0.5)
+		await fader.step_finished
+		
+		# Exit the old scene and enter the new
+		if current_scene:
+			current_scene.exit()
+			await current_scene.tree_exited
+		new_scene.sys = self
+		new_scene.enter()
+		call_deferred("add_child", new_scene)
+		current_scene = new_scene
+		
+		# Fade out the cover
+		await fader.finished
+		cover.visible = false
+	else:
+		print("ERROR: Address isn't of type: 'Screen'!")
